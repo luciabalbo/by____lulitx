3472a7dade9496a8844a3f7ba6c80f5445cfe61d
Lula <lucia.balbo@mi.unc.edu.ar>
2026-02-28 18:49:29 -0300

    Cargando productos

--- app/api/productos/route.ts
+++ app/api/productos/route.ts
@@ -14,7 +14,7 @@ const INITIAL_PRODUCTS = [
   },
   {
     name: "SHORT INTERVENIDO_v44",
-    price: 27000,
+    price: 25000,
     description: "CONSTRUCCIÓN HÍBRIDA SHORT+SHORT. CINTURA 88CM. TALLE 44. PIEZA DE ARCHIVO ÚNICA.",
     images: ['/productos/2.jpg', '/productos/02.png', '/productos/20.png'],
     tag: "FILE_02"
@@ -27,25 +27,46 @@ const INITIAL_PRODUCTS = [
     tag: "FILE_03"
   },
   {
-    name: "BUZO A_ctrlX",
-    price: 67000,
-    description: "CONSTRUCCIÓN BUZO+CADENAS. TALLE M. PIEZA DE ARCHIVO ÚNICA.",
-    images: ['/productos/44.png', '/productos/04.png', '/productos/40.png'],
+    name: "PANTALÓN P_ctrlO",
+    price: 70000,
+    description: "CONSTRUCCIÓN JEAN_AZUL_BASE+JEAN_GRIS+LLAVEROS. CINTURA 88CM. TALLE 44. LARGO TOTAL 105COM. PIEZA DE ARCHIVO ÚNICA.",
+    images: ['/productos/55.jpg', '/productos/05.png','/productos/5.jpg'],
     tag: "FILE_04"
   },
   {
-    name: "PANTALÓN P_ctrlO",
-    price: 70000,
-    description: "CONSTRUCCIÓN PANTALÓN+LLAVEROS. CINTURA 88CM. TALLE 44. LARGO TOTAL 105COM. PIEZA DE ARCHIVO ÚNICA.",
-    images: ['/productos/55.jpg', '/productos/05.png','/productos/5.jpg'],
+    name: "BUZO A_ctrlX",
+    price: 67000,
+    description: "CONSTRUCCIÓN BUZO_ADIDAS+CADENAS. TALLE M. PIEZA DE ARCHIVO ÚNICA.",
+    images: ['/productos/44.png', '/productos/04.png', '/productos/40.png'],
     tag: "FILE_05"
   },
   {
     name: "PANTALÓN P_ctrlC",
     price: 67000,
-    description: "CONSTRUCCIÓN PANTALÓN+ARGOLLAS_DORADAS. CINTURA 72CM. TALLE 36. PIEZA DE ARCHIVO ÚNICA.",
+    description: "CONSTRUCCIÓN JEAN_AZUL_BASE+JEAN_CELESTE+ARGOLLAS_DORADAS. CINTURA 72CM. TALLE 36. PIEZA DE ARCHIVO ÚNICA.",
     images: ['/productos/6.jpg', '/productos/06.png','/productos/60.png', '/productos/66.jpg'],
     tag: "FILE_06"
+  },
+  {
+    name: "PANTALÓN P_ctrlY",
+    price: 65000,
+    description: "CONSTRUCCIÓN JEAN_NEGRO_BASE+JEAN_AZUL. CINTURA 72CM. TALLE 36. PIEZA DE ARCHIVO ÚNICA.",
+    images: ['/productos/7.jpg', '/productos/07.png','/productos/70.png', '/productos/77.jpg'],
+    tag: "FILE_07"
+  },
+  {
+    name: "POLLERA :))))",
+    price: 27000,
+    description: "CONSTRUCCIÓN HÍBRIDA + PINTURA. CINTURA 76CM . TALLE 38. PIEZA DE ARCHIVO ÚNICA.",
+    images: ['/productos/9.png', '/productos/09.png','/productos/90.png'],
+    tag: "FILE_08"
+  },
+  {
+    name: "CHALECO ctrlC_ctrlV",
+    price: 33000,
+    description: "CONSTRUCCIÓN HÍBRIDA. TALLE L. PIEZA DE ARCHIVO ÚNICA.",
+    images: ['/productos/8.jpg', '/productos/08.png','/productos/80.png', '/productos/88.jpg'],
+    tag: "FILE_09"
   },
   // --- PEGÁ LOS OTROS 16 ACÁ ABAJO ---
   // { name: "PRENDA 5", price: 1000, ... },
