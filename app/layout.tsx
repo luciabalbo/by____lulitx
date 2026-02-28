4c501f30c22bce3fe47ece89d28f9a984d306360
Lula <lucia.balbo@mi.unc.edu.ar>
2026-02-28 17:12:25 -0300

    Correccion de tipografia en celulares

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 // app/layout.tsx
-import { Syne, JetBrains_Mono } from 'next/font/google'
+import { Syne, Courier_Prime } from 'next/font/google' // Importamos Courier_Prime
 
 const syne = Syne({ 
   weight: ['400', '700', '800'],
@@ -7,14 +7,16 @@ const syne = Syne({
   variable: '--font-syne' 
 })
 
-const jetbrains = JetBrains_Mono({ 
+// Configuramos Courier Prime para que sea tu fuente mono oficial
+const courier = Courier_Prime({ 
+  weight: ['400', '700'],
   subsets: ['latin'], 
-  variable: '--font-mono' 
+  variable: '--font-courier' 
 })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${syne.variable} ${jetbrains.variable}`}>
+    <html lang="en" className={`${syne.variable} ${courier.variable}`}>
       <body style={{ margin: 0, background: '#000' }}>{children}</body>
     </html>
   )
