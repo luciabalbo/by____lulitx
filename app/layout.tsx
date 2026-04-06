11979fa7a6baa92fb1fb3479ff574d8c0ca34c0a
Lula <lucia.balbo@mi.unc.edu.ar>
2026-04-06 10:43:24 -0300

    Agregando etiqueta de metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Head from 'next/head' // <-- IMPORTAMOS EL HEAD DE NEXT
 
 const inter = Inter({ 
   subsets: ['latin'], 
@@ -9,6 +10,10 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <meta name="p:domain_verify" content="0373d53777a4c2143eff1e389c6db190"/>
+      </head>
+      
       <body style={{ margin: 0, background: '#000' }}>
         {children}
       </body>
